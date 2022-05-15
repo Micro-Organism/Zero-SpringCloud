0e823aa056f5808ca57aa528b32bc0030c72affd
Micro丶Organism <17310708550@163.com>
2022-05-15 22:17:46 +0800

    initial commit

---
+++ Zero-Cloud/src/main/java/com/zero/zerocloud/ZeroCloudApplication.java
@@ -0,0 +1,13 @@
+package com.zero.zerocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroCloudApplication.class, args);
+    }
+
+}
---
+++ Zero-Cloud/src/test/java/com/zero/zerocloud/ZeroCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.zerocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
